96df8d42d1d6bf2d40ccabe0e725e36defe7ed26
Steve B <sbogucki@mail.usf.edu>
2020-01-08 14:09:15 -0800

    first deploy to azure

---
+++ build/precache-manifest.f8c9a9172724d3ab198e3572e89279ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f82a16b9e9853feb35dcab60d0e350",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14cb1cd4bfdbfb6d45b1",
+    "url": "/static/css/main.d2ae15c1.chunk.css"
+  },
+  {
+    "revision": "61f69d51ac5f097db810",
+    "url": "/static/js/2.c324ceed.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.c324ceed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14cb1cd4bfdbfb6d45b1",
+    "url": "/static/js/main.53b0e35c.chunk.js"
+  },
+  {
+    "revision": "4358885dffcd60e9ada7",
+    "url": "/static/js/runtime-main.7f9fd0e3.js"
+  },
+  {
+    "revision": "ae4e94c69cf24a229bc77a62816933ea",
+    "url": "/static/media/gmailIcon.ae4e94c6.png"
+  },
+  {
+    "revision": "0e84d5926187e7a3b785febd3a55bf1d",
+    "url": "/static/media/igicon.0e84d592.png"
+  }
+]);
