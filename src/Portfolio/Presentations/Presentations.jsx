02d3e7b563d6b033cd0721c120cacc6e0b303933
Steve B <sbogucki@mail.usf.edu>
2020-01-10 21:01:24 -0800

    added presentations component

--- src/Portfolio/Presentations/Presentations.jsx
+++ src/Portfolio/Presentations/Presentations.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './presentations.css';
-import * as constants from '../../Utils/consts';
+//import * as constants from '../../Utils/consts';
 
 const Presentations = () => {
  
   return (   
-      <div className="portfolio-container">
-        <div className="portfolio-btn-row">
-            <h1>
-                {constants.PRESENTATIONS}
-            </h1>
+      <div className="presentations-container">
+        <div className="presentations-card-container">
+            <div className="presentation-card-top">
+                <i class="far fa-file-powerpoint"></i>
+            </div>
+            <div className="presentation-card-title">
+                Title
+            </div>
+            <div className="presentation-card-caption">
+                Caption
+            </div>
+            <div className="presentation-card-bottom">
+                <i class="fas fa-download"></i>
+            </div>
         </div>
+        
         <div className="portfolio-back-btn-container">
           <Link to="/portfolio">
             <div>
